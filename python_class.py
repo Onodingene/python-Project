ade98c9daca823bd6e4b668207f95d90bc74bfa6
Onodingene Somtochukwu <mimi.somto1@gmail.com>
2023-03-29 16:31:31 +0100

    1st committ

---
+++ python_class.py
@@ -0,0 +1,16 @@
+# car = "Mercedez- Benz"
+# print(car)
+# car = "Lexus"
+# print("car")  -- This shows that variables dont have quotes and if you put print sth in quotes then it would print what is in the quote if not it in quotes then it would print the name you assigned to the variable
+
+# lunch = 'burgers'
+# print(lunch)
+# lunch = 'rice'
+# print(lunch) -- This would print out both burger and rice cause you can re-assign variables in python
+# and it is called over-writing
+
+# lunch, book = "burger", "my lovely girl"
+# print(f"{lunch} is my favourite meal of the day! as well as ,{book}is my fav book") -- explain the formatting.i.e usinf f
+
+# cap = 5 * 2 + 4
+# print(cap) -- This is for BODMAS
