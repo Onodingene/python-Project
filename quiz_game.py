285a416ac3afa6a0d5f9b6c7dc8d08d20dc8a9b0
Onodingene Somtochukwu <mimi.somto1@gmail.com>
2023-04-01 01:01:36 +0100

    quiz game

--- quiz_game.py
+++ quiz_game.py
@@ -1,45 +1,48 @@
-#this game asks users a bunch of questions and if they get them, 1 mark is added to their score
+#This game asks users a bunch of questions and if they get them, 1 mark is added to their score
 print("Welcome to Somies Quiz!")
-game= input("Do you want to play Somie's Quiz Game- ")
+game = input("Do you want to play Somie's Quiz Game- ")
 if game.lower() == "yes":
     print("Lets begin the game!")
-    answer1 = "2"
-    question1 = input("How many colours are on the Nigerian flag- ")
-    if question1 == answer1:
-        print("Your answer is correct!")
-    else:
-        print(f"Your answer is wrong. The right answer is {answer1}")
-
-    answer1 = "Britian"
-    answer2 = "Great Britian"
-    question1 = input("Which country colonized Nigeria- ")
-    if question1.lower() == answer1 or question1.lower() == answer2:
-            print("Your answer is correct!")
-    else:
-            print(f"Your answer is wrong. The right answer is {answer1}")
-
-    answer1 = "strength"
-    question1 = input("What does the horse on the Nigerian coat of arms represent?")
-    if question1.lower() == answer1:
-        print("Your answer is correct!")
-    else:
-        print(f"Your answer is wrong. The right answer is {answer1}")
-
-    #answer1 = "No"
-    # question1 = input("Is it right to murder someone?")
-    #     if question1 == answer1:
-    #         print("Your answer is correct!")
-    #     else:
-    #         print(f"Your answer is wrong. The right answer is {answer1}")
-    #
-    # answer1 = "36"
-    # answer2 = "36 states"
-    # question1 = input("How many states are in Nigeria?")
-    # if question1 == answer1 or question1 == answer2:
-    #     print("Your answer is correct!")
-    # else:
-    #     print(f"Your answer is wrong. The right answer is {answer1}")
-
-
+    score = 0
 else:
     print("The game ends here!")
+    quit()
+
+question1 = input("How many colours are on the Nigerian flag- ")
+answer = "2"
+if question1 == answer:
+    print("Your answer is correct!")
+    score += 1
+else:
+    print(f"Your answer is wrong. The right answer is {answer}")
+
+question2= input("Which country colonized Nigeria- ")
+answer = "britian"
+if question2.lower() == answer:
+    print("Your answer is correct!")
+    score +=1
+else:
+    print(f"Your answer is wrong. The right answer is {answer}")
+
+question3= input("What does the horse on the Nigerian coat of arms represent?")
+answer = "strength"
+if question3.lower() == answer:
+    print("Your answer is correct!")
+    score+=1
+else:
+    print(f"Your answer is wrong. The right answer is {answer}")
+
+question4 = input("Is it right to murder someone?")
+answer= "no"
+if question4.lower() == answer:
+    print("Your answer is correct!")
+    score+=1
+else:
+    print(f"Your answer is wrong. The right answer is {answer}")
+
+print("This brings us to the end of the quiz!")
+if str(score) >= "3":
+    print(f"You did Great, Your score is  "+ str(score)+ "  over 4 ")
+else:
+    print(f"You can do better! Your score is  "+ str(score)+ "  over 4 ")
+
